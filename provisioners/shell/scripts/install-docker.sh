9e666b8a6329c0897b99c674edaee850fee94e6c
Chris Olstrom <chris@olstrom.com>
2017-08-10 14:44:26 -0700

    Enabling the docker service seems useful...

--- provisioners/shell/scripts/install-docker.sh
+++ provisioners/shell/scripts/install-docker.sh
@@ -6,3 +6,5 @@ set -o xtrace
 zypper addrepo --check --gpgcheck --refresh obs://Virtualization:containers Virtualization:containers
 zypper --gpg-auto-import-keys refresh
 zypper --non-interactive install --from Virtualization:containers docker
+
+systemctl enable docker.service
